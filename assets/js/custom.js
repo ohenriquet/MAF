8e11248a3f9f247a590e9af0ab6bed63fc2a4f2b
ohenriquet <ohenriquet@gmail.com>
2024-02-07 16:52:05 -0300

    Homepage
    Praticamente pronta

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function ($) {
 //Set the carousel options
 	$('#quote-carousel').carousel({
 		pause: true,
-		interval: 4000
+		interval: 14000
 	});
 // fancybox
     $(".fancybox").fancybox();
